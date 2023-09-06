34831746fba31cd058ab31c3521936fe89d80bcf
newxxson <hyukjun1111@gmail.com>
2023-09-06 22:23:06 +0900

    changed import style

--- views/game/makeQrCode.js
+++ views/game/makeQrCode.js
@@ -1,5 +1,5 @@
-import { QRCode } from "qrcode";
-
+import pkg from "qrcode";
+const { QRCode } = pkg;
 export default async function makeQrCode(roomId) {
   const baseUrl =
     "https://port-0-badgeback-jvvy2blm6d8yj1.sel5.cloudtype.app/api/game/join-game/";
